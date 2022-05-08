fffdc1142e106f52165c0edfebea8a3567ad7437
Rafin_31 <asifhossain976@gmail.com>
2022-05-08 22:31:18 +0600

    JWT added V2

--- index.js
+++ index.js
@@ -21,6 +21,20 @@ const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology:
 
 //JWT Token
 
+function jwtTokenVerify(req, res, next) {
+    const authHeader = req.headers.authorization;
+    if (!authHeader) {
+        return res.status(401).send({ Response: 'Unauthorized Access' });
+    }
+    const token = authHeader.split(' ')[1];
+    jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, decoded) => {
+        if (err) {
+            return res.status(403).send({ Response: 'Forbidden access' });
+        }
+        req.decoded = decoded;
+        next();
+    })
+}
 
 
 
